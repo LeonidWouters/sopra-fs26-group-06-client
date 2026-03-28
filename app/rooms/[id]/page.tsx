de162e470f145299192937f5fd8749c2b877e1fb
Raffael Bischoff <raffaelstefan.bischoff@uzh.ch>
2026-03-28 16:19:06 +0100

    add leave room button, updates backend/reroutes mainpage

--- app/rooms/[id]/page.tsx
+++ app/rooms/[id]/page.tsx
@@ -1,13 +1,33 @@
 "use client";
 
 import React, {useEffect, useRef} from 'react';
-import { useParams } from 'next/navigation';
+import {useParams, useRouter} from 'next/navigation';
+import {Button} from "antd";
+import {useApi} from "@/hooks/useApi";
+import {useAuth} from "@/hooks/useAuth";
+import useLocalStorage from "@/hooks/useLocalStorage";
+
+
 
 
 const RoomPage: React.FC = () => {
+    const router = useRouter();
+    const apiService = useApi();
+    const { token, isReady } = useAuth();
     const { id } = useParams();
+
+    const {
+        clear: clearToken,
+    } = useLocalStorage<string>("token", ""); // if you wanted to select a different token, i.e "lobby", useLocalStorage<string>("lobby", "");
+
+    const leaveRoom = (): void => {
+        // Clear token using the returned function 'clear' from the hook
+        apiService.put(`/rooms/${id}/leave`, null, token);
+        router.push("/mainpage");
+    };
     const videoRef = useRef(null);
     useEffect(() => {
+        if(!isReady) return; //ensure token is loaded
         let Stream;
         const startMediaDevice = async() =>{
             try {
@@ -19,13 +39,17 @@ const RoomPage: React.FC = () => {
            }
         }
         startMediaDevice();
-    }, []);
+    }, [apiService, token, isReady]);
     return (
-        <div
-            style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", color: "black"}}
-        >
-            Room {id}
-            <video ref ={videoRef} autoPlay muted />
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", color: "blueviolet"}} >
+            <div>
+            <video ref ={videoRef} autoPlay={true} muted />
+            </div>
+            <div>
+                <Button onClick={leaveRoom} type="primary">Leave Video Call</Button>
+            </div>
+
+
         </div>
     );
 };
