7cff7c9039818e0e305a06580262071d63adaba3
silvan <silvanoliver.mueller@uzh.ch>
2026-03-29 20:17:52 +0200

    profile page new design and change password logic/design   (#9 and #15)   - new profile.module.css with card and avatar styles   - profile page split into 3 cards: profile info, edit profile, change password   - status badge shows grey when offline   - removed cancel button and handleCancelEdit   - error messages use messageApi consistently   - navbar reused from mainpage styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,16 +34,16 @@ export default function RootLayout({
               // general theme options are set in token, meaning all primary elements (button, menu, ...) will have this color
               colorPrimary: "#22426b", // selected input field boarder will have this color as well
               borderRadius: 8,
-              colorText: "#fff",
+              colorText: "#171717",
               fontSize: 16,
 
               // Alias Token
-              colorBgContainer: "#16181D",
+              colorBgContainer: "#ffffff",
             },
             // if a component type needs special styling, setting here will override default options set in token
             components: {
               Button: {
-                colorPrimary: "#75bd9d", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
+                colorPrimary: "#6B21D6", // this will color all buttons in #75bd9d, overriding the default primaryColor #22426b set in token line 35
                 algorithm: true, // enable algorithm (redundant with line 33 but here for demo purposes)
                 controlHeight: 38,
               },
@@ -53,7 +53,7 @@ export default function RootLayout({
                 algorithm: false, // disable algorithm (line 32)
               },
               Form: {
-                labelColor: "#fff",
+                labelColor: "#364153",
                 algorithm: theme.defaultAlgorithm, // specify a specifc algorithm instead of true/false
               },
               Card: {},
