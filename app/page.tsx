4bb4024933af716c9fd04706011dedef57074eb1
Tiago Afonso Caselas <tiago.afonsocaselas@uzh.ch>
2026-03-30 09:29:44 +0200

    Added a user overview and search function for all registered users

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ const Login: React.FC = () => {
             if (response.id) {
                 setId(String(response.id));
             }
-            router.push(`/mainpage/${response.id}`);
+            router.push(`/mainpage/`);
         } catch (error) {
             if (
                 error instanceof Error &&
--- app/types/user.ts
+++ app/types/user.ts
@@ -2,6 +2,7 @@ export interface User {
   id: string | null;
   password: string | null;
   username: string | null;
+  name: string | null;
   token: string | null;
   status: string | null;
   bio: string | null;
