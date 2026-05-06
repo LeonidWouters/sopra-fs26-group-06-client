c65005c3e24453b11b82d6215ee4d373326bb07b
Raffael Bischoff <raffaelstefan.bischoff@uzh.ch>
2026-05-06 20:29:58 +0200

    implements client side heartbeat ping that gets sent every 30 seconds

--- app/rooms/[id]/page.tsx
+++ app/rooms/[id]/page.tsx
@@ -299,6 +299,19 @@ const RoomPage: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [incomingOffer, isMediaReady]);
 
+    function sendHeartbeat(token: string) {
+        apiService.put(`/rooms/${id}/heartbeat`, null, token);
+    }
+
+    useEffect(() =>{
+        if(!callStarted) return;
+        sendHeartbeat(token);
+        const interval = setInterval(() => {
+            sendHeartbeat(token);
+        },30000)
+        return () => clearInterval(interval);
+    }, [callStarted]);
+
     useEffect(() => {
         if (!isReady) return;
         const baseDomain = getApiDomain().replace(/^http/, "ws").replace(/\/$/, "");
@@ -532,7 +545,6 @@ const RoomPage: React.FC = () => {
 
     function startTTT() {
         setChat(true);//turns on chat feature so it is visible
-
     }
 
     function startSTT() {
